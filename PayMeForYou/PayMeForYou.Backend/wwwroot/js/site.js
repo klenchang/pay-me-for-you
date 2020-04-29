8433c6eb46e43843cf5d668268d64075eeaf5b98
Klen <klenchang0313@gmail.com>
2020-04-29 16:28:19 +0800

    commit js

---
+++ PayMeForYou/PayMeForYou.Backend/wwwroot/js/site.js
@@ -0,0 +1,48 @@
+﻿// Get the Sidebar
+var mySidebar = document.getElementById("mySidebar");
+
+// Get the DIV with overlay effect
+var overlayBg = document.getElementById("myOverlay");
+
+// Toggle between showing and hiding the sidebar, and add overlay effect
+function switchMenu() {
+    var display = mySidebar.style.display === 'block' ? 'none' : 'block';
+    mySidebar.style.display = display;
+    overlayBg.style.display = display;
+}
+
+// Show and hide side bar item
+function switchSideBarItemDetail(trigger) {
+    var x = trigger.nextElementSibling;
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+        x.previousElementSibling.className += " w3-green";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+        x.previousElementSibling.className =
+            x.previousElementSibling.className.replace(" w3-green", "");
+    }
+}
+
+$(".partial-load").click(event => {
+    event.preventDefault();
+    var url = $(event.target).attr("href");
+    $('#divMainBody').load(url);
+    if ($("#btnMenu").css("display") === 'block') {
+        w3_close();
+    }
+});
+
+//$("form").submit(function(e) {
+//    console.log(e);
+//    var form = $(this);
+//    $.ajax({
+//        url: form.attr('action'),
+//        type: form.attr('method'),
+//        data: form.serialize(), // data to be submitted
+//        success: function(response) {
+//            alert(response); // do what you like with the response
+//        }
+//    });
+//    return false;
+//});
